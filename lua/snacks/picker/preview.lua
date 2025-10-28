e211eb3e4e6eed6ab03c199a01a9f0c1ed42c8e5
Tu Nguyen <minhtu1392000@gmail.com>
2025-10-28 10:14:47 +0700

    fix(picker): avoid using loaded buffer as preview to prevent keymaps conflict

--- lua/snacks/picker/preview.lua
+++ lua/snacks/picker/preview.lua
@@ -95,80 +95,73 @@ function M.file(ctx)
     vim.fn.bufload(ctx.item.buf)
   end
 
-  if ctx.item.buf and vim.api.nvim_buf_is_loaded(ctx.item.buf) then
-    local name = vim.api.nvim_buf_get_name(ctx.item.buf)
-    name = uv.fs_stat(name) and vim.fn.fnamemodify(name, ":t") or name
-    ctx.preview:set_title(name)
-    ctx.preview:set_buf(ctx.item.buf)
-  else
-    local path = Snacks.picker.util.path(ctx.item)
-    if not path then
-      ctx.preview:notify("Item has no `file`", "error")
-      return
+  local path = Snacks.picker.util.path(ctx.item)
+  if not path then
+    ctx.preview:notify("Item has no `file`", "error")
+    return
+  end
+
+  if Snacks.image.supports_file(path) then
+    return M.image(ctx)
+  end
+
+  -- re-use existing preview when path is the same
+  if path ~= Snacks.picker.util.path(ctx.prev) then
+    ctx.preview:reset()
+    vim.bo[ctx.buf].buftype = ""
+
+    local name = vim.fn.fnamemodify(path, ":t")
+    ctx.preview:set_title(ctx.item.title or name)
+
+    local stat = uv.fs_stat(path)
+    if not stat then
+      ctx.preview:notify("file not found: " .. path, "error")
+      return false
+    end
+    if stat.type == "directory" then
+      return M.directory(ctx)
+    end
+    local max_size = ctx.picker.opts.previewers.file.max_size or (1024 * 1024)
+    if stat.size > max_size then
+      ctx.preview:notify("large file > 1MB", "warn")
+      return false
+    end
+    if stat.size == 0 then
+      ctx.preview:notify("empty file", "warn")
+      return false
     end
 
-    if Snacks.image.supports_file(path) then
-      return M.image(ctx)
+    local file = assert(io.open(path, "r"))
+
+    local is_binary = false
+    local ft = ctx.picker.opts.previewers.file.ft or vim.filetype.match({ filename = path })
+    if ft == "bigfile" then
+      ft = nil
     end
-
-    -- re-use existing preview when path is the same
-    if path ~= Snacks.picker.util.path(ctx.prev) then
-      ctx.preview:reset()
-      vim.bo[ctx.buf].buftype = ""
-
-      local name = vim.fn.fnamemodify(path, ":t")
-      ctx.preview:set_title(ctx.item.title or name)
-
-      local stat = uv.fs_stat(path)
-      if not stat then
-        ctx.preview:notify("file not found: " .. path, "error")
-        return false
+    local lines = {}
+    for line in file:lines() do
+      ---@cast line string
+      if #line > ctx.picker.opts.previewers.file.max_line_length then
+        line = line:sub(1, ctx.picker.opts.previewers.file.max_line_length) .. "..."
       end
-      if stat.type == "directory" then
-        return M.directory(ctx)
-      end
-      local max_size = ctx.picker.opts.previewers.file.max_size or (1024 * 1024)
-      if stat.size > max_size then
-        ctx.preview:notify("large file > 1MB", "warn")
-        return false
-      end
-      if stat.size == 0 then
-        ctx.preview:notify("empty file", "warn")
-        return false
-      end
-
-      local file = assert(io.open(path, "r"))
-
-      local is_binary = false
-      local ft = ctx.picker.opts.previewers.file.ft or vim.filetype.match({ filename = path })
-      if ft == "bigfile" then
-        ft = nil
-      end
-      local lines = {}
-      for line in file:lines() do
-        ---@cast line string
-        if #line > ctx.picker.opts.previewers.file.max_line_length then
-          line = line:sub(1, ctx.picker.opts.previewers.file.max_line_length) .. "..."
+      -- Check for binary data in the current line
+      if line:find("[%z\1-\8\11\12\14-\31]") then
+        is_binary = true
+        if not ft then
+          ctx.preview:notify("binary file", "warn")
+          return
         end
-        -- Check for binary data in the current line
-        if line:find("[%z\1-\8\11\12\14-\31]") then
-          is_binary = true
-          if not ft then
-            ctx.preview:notify("binary file", "warn")
-            return
-          end
-        end
-        table.insert(lines, line)
       end
-
-      file:close()
-
-      if is_binary then
-        ctx.preview:wo({ number = false, relativenumber = false, cursorline = false, signcolumn = "no" })
-      end
-      ctx.preview:set_lines(lines)
-      ctx.preview:highlight({ file = path, ft = ctx.picker.opts.previewers.file.ft, buf = ctx.buf })
+      table.insert(lines, line)
     end
+
+    file:close()
+
+    if is_binary then
+      ctx.preview:wo({ number = false, relativenumber = false, cursorline = false, signcolumn = "no" })
+    end
+    ctx.preview:set_lines(lines)
+    ctx.preview:highlight({ file = path, ft = ctx.picker.opts.previewers.file.ft, buf = ctx.buf })
   end
   ctx.preview:loc()
 end
